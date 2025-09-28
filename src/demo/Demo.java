f0d9dd979df7aa7acd5e724378ee7dcf85e848f4
JorgeCarrilesRuiz <uo294721@uniovi.es>
2025-09-28 18:29:59 +0200

    commit for conflict 2

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 1");
+		System.out.println("cambio 3 en luna");
 		System.out.println("linea 1");
 		System.out.println("branch_1");
 	}
